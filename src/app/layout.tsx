fb18455399e4189abd2713193ca20c1f6855a2a0
varshini-edgehax <varshini@edgehax.com>
2026-06-22 00:55:32 +0530

    feat: add sitemap.xml and robots.txt for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,6 +78,7 @@ export default function RootLayout({
     <html lang="en" className={`${bebasNeue.variable} ${montserrat.variable}`}>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="Dr. Manjunatha C" />
